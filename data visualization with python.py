56bb2ba9c564e2e6b99f0cd0e87ace871cc08e3f
Vishwa Moorthy <vishwa@Vishwas-MacBook-Pro.local>
2022-08-29 01:55:11 +0800

    very cool

--- data visualization with python.py
+++ data visualization with python.py
@@ -2,8 +2,20 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-x = [1,2,3]
-y = [1,3,4]
 
+x = [1,2,3]
+#dispaly y axis for first graph 
+y = [1,3,4]
+#display z axis for second graph 
+z = [10,5,0]
+
+#x,y graph 
 plt.plot(x,y)
+#x, z graph 
+plt.plot(x,z)
+plt.title("Testing plot")
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.legend(['this is y axis','this is z'])
 plt.show()
